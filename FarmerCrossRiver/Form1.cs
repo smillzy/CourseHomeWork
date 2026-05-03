d6f7a4bfc6edcd45135977e026c4440618b76778
phoebe <mongzhen0856@gmail.com>
2026-05-03 17:02:28 +0800

    Farmer cross river

--- FarmerCrossRiver/Form1.cs
+++ FarmerCrossRiver/Form1.cs
@@ -38,25 +38,137 @@ namespace FarmerCrossRiver
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedItems != null)
-            {
-                string item = (string)listBox1.SelectedItem;
-                _leftList.Remove(item);
-                _rightList.Add(item);
-                ChangeData();
-            }
-
+            MoveItems(listBox1, _leftList, _rightList);
         }
+       
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (listBox2.SelectedItem != null)
+            MoveItems(listBox2, _rightList, _leftList);
+        }
+
+        private void MoveItems(ListBox listBox, List<string> fromList, List<string> toList)
+        {
+            if (!SomeThingHasBeenChosen(listBox))
             {
-                string item = (string)listBox2.SelectedItem;
-                _rightList.Remove(item);
-                _leftList.Add(item);
-                ChangeData();
+                return;
             }
+
+            List<string> selectedItems = listBox.SelectedItems.Cast<string>().ToList();
+
+            if (!IsValidMove(selectedItems))
+            {
+                MessageBox.Show("農夫一定要同行，而且小船最多只能再帶一樣東西。");
+                return;
+            }
+
+            foreach (string item in selectedItems)
+            {
+                fromList.Remove(item);
+                toList.Add(item);
+            }
+
+            ChangeData();
+
+            if (!IsSafe())
+            {
+                GameOver("你輸了！");
+                return;
+            }
+
+            if (IsWin())
+            {
+                GameOver("恭喜，全部成功過河！");
+                return;
+            }
+        }
+
+        private bool SomeThingHasBeenChosen(ListBox listBox)
+        {
+            if (listBox.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("請先選擇要移動的");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidMove(List<string> selectedItems)
+        {
+            if (!selectedItems.Contains("農夫"))
+            {
+                return false;
+            }
+
+            if (selectedItems.Count > 2)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsSafe()
+        {
+            return IsSideSafe(_leftList) && IsSideSafe(_rightList);
+        }
+
+        private bool IsSideSafe(List<string> side)
+        {
+            bool hasFarmer = side.Contains("農夫");
+            bool hasWolf = side.Contains("狼");
+            bool hasSheep = side.Contains("羊");
+            bool hasVegetable = side.Contains("菜");
+
+            if (!hasFarmer && hasWolf && hasSheep)
+            {
+                return false;
+            }
+
+            if (!hasFarmer && hasSheep && hasVegetable)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void GameOver(string message)
+        {
+            DialogResult result = MessageBox.Show(
+                message + "\n\n是否要重新開始？",
+                "遊戲結束",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                RestartGame();
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+        }
+
+        private void RestartGame()
+        {
+            CreateList();
+            ChangeData();
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private bool IsWin()
+        {
+            return _rightList.Contains("農夫") &&
+                   _rightList.Contains("狼") &&
+                   _rightList.Contains("羊") &&
+                   _rightList.Contains("菜");
         }
     }
 }
